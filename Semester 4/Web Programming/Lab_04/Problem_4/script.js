fafb10e816556d1e036a61d0d24c216714ee98ba
Nicolae Mogage <mogagenicolae@yahoo.com>
2023-04-22 17:41:20 +0300

    Added problem 4

--- Semester 4/Web Programming/Lab_04/Problem_4/script.js
+++ Semester 4/Web Programming/Lab_04/Problem_4/script.js
@@ -1,4 +1,4 @@
-function sortTable(lineNumber) {
+function sortTableColumns(colNumber) {
     let table = document.getElementById("table01");
     let rows = table.rows;
     let columns = [];
@@ -12,29 +12,29 @@ function sortTable(lineNumber) {
     }
 
     function ascending(a, b) {
-        if (a[lineNumber] < b[lineNumber]) {
+        if (a[colNumber] < b[colNumber]) {
             return -1;
         }
-        if (a[lineNumber] > b[lineNumber]) {
+        if (a[colNumber] > b[colNumber]) {
             return 1;
         }
         return 0;
     }
 
     function descending(a, b) {
-        if (a[lineNumber] > b[lineNumber]) {
+        if (a[colNumber] > b[colNumber]) {
             return -1;
         }
-        if (a[lineNumber] < b[lineNumber]) {
+        if (a[colNumber] < b[colNumber]) {
             return 1;
         }
         return 0;
     }
 
     let before = columns.toString();
-    if (!isNaN(parseInt(columns[0][lineNumber]))) {
+    if (!isNaN(parseInt(columns[0][colNumber]))) {
         for (let index = 0; index < columns.length; index++) {
-            columns[index][lineNumber] = parseInt(columns[index][lineNumber]);
+            columns[index][colNumber] = parseInt(columns[index][colNumber]);
         }
     }
     columns.sort(ascending);
@@ -46,5 +46,52 @@ function sortTable(lineNumber) {
         for (let nthChild = 0; nthChild < rows[index].children.length - 1; nthChild++) {
             rows[index].children[nthChild + 1].querySelector("div").innerHTML = columns[nthChild][index];
         }
+    }
+}
+
+function sortTableRows(lineNumber) {
+    let table = document.getElementById("table02");
+    let rows = table.rows;
+
+    function ascending(a, b) {
+        if (a.sortValue < b.sortValue) {
+            return -1;
+        }
+        if (a.sortValue > b.sortValue) {
+            return 1;
+        }
+        return 0;
+    }
+
+    function descending(a, b) {
+        if (a.sortValue > b.sortValue) {
+            return -1;
+        }
+        if (a.sortValue < b.sortValue) {
+            return 1;
+        }
+        return 0;
+    }
+
+    let toSort = [];
+    for (let index = 1; index < rows.length; index++) {
+        let toAdd = [rows[index].innerHTML];
+        toAdd.sortValue = rows[index].children[lineNumber].querySelector("div").innerHTML;
+        toSort.push(toAdd);
+    }
+
+    let before = toSort.toString();
+    if (!isNaN(parseInt(toSort[0].sortValue))) {
+        for (let index = 0; index < toSort.length; index++) {
+            toSort[index].sortValue = parseInt(toSort[index].sortValue);
+        }
+    }
+    toSort.sort(ascending);
+    if (before === toSort.toString()) {
+        toSort.sort(descending);
+    }
+
+    for (let index = 0; index < rows.length - 1; index++) {
+        rows[index + 1].innerHTML = toSort[index];
     }
 }
