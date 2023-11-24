114f348e56f12bc8e7852120bde93e75fc123d08
Nicolae Mogage <mogagenicolae@yahoo.com>
2023-11-24 02:30:33 +0200

    Added new datasets and models

--- Semester 5/Research Project/Video_Project/main.py
+++ Semester 5/Research Project/Video_Project/main.py
@@ -1,138 +1,133 @@
-import numpy as np
 import torch
-import torch.nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-
-import pytorch_lightning as pl
-
-from torch_enhance.datasets import BSDS300, Set14, Set5
-from torch_enhance.models import SRResNet
-from torch_enhance import metrics
-
-from skimage.metrics import structural_similarity as ssim
+import torch.nn as nn
 
 
-class Module(pl.LightningModule):
-
-    def __init__(self, model):
+class DoubleConvBlock(nn.Module):
+    """double conv layers block"""
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.model = model
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        return self.model(x)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
-
-    def training_step(self, batch, batch_idx):
-        lr, hr = batch
-        sr = self(lr)
-        loss = F.mse_loss(sr, hr, reduction="mean")
-
-        # metrics
-        mae = metrics.mae(sr, hr)
-        psnr = metrics.psnr(sr, hr)
-
-        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-
-        ssim_value = np.mean(np.array([
-            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
-                 use_sample_covariance=False, win_size=3,
-                 data_range=sr_i.max() - sr_i.min())
-            for hr_i, sr_i in zip(hr_np, sr_np)
-        ]))
-
-        # Logs
-        self.log("train_loss", loss)
-        self.log("train_mae", mae)
-        self.log("train_psnr", psnr)
-        self.log("train_ssim", ssim_value)
-
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        lr, hr = batch
-        sr = self(lr)
-        loss = F.mse_loss(sr, hr, reduction="mean")
-
-        # metrics
-        mae = metrics.mae(sr, hr)
-        psnr = metrics.psnr(sr, hr)
-
-        # Logs
-        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-
-        ssim_value = np.mean(np.array([
-            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
-                 use_sample_covariance=False, win_size=3,
-                 data_range=sr_i.max() - sr_i.min())
-            for hr_i, sr_i in zip(hr_np, sr_np)
-        ]))
-
-        # Logs
-        self.log("train_loss", loss)
-        self.log("train_mae", mae)
-        self.log("train_psnr", psnr)
-        self.log("train_ssim", ssim_value)
-
-        return loss
-
-    def test_step(self, batch, batch_idx):
-        lr, hr = batch
-        sr = self(lr)
-        loss = F.mse_loss(sr, hr, reduction="mean")
-
-        # metrics
-        mae = metrics.mae(sr, hr)
-        psnr = metrics.psnr(sr, hr)
-
-        # Logs
-        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
-
-        ssim_value = np.mean(np.array([
-            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
-                 use_sample_covariance=False, win_size=3,
-                 data_range=sr_i.max() - sr_i.min())
-            for hr_i, sr_i in zip(hr_np, sr_np)
-        ]))
-
-        # Logs
-        self.log("train_loss", loss)
-        self.log("train_mae", mae)
-        self.log("train_psnr", psnr)
-        self.log("train_ssim", ssim_value)
-
-        return loss
+        return self.double_conv(x)
 
 
-scale_factor = 4
+class DownBlock(nn.Module):
+    """Downscale block: maxpool -> double conv block"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConvBlock(in_channels, out_channels)
+        )
 
-# Setup dataloaders
-train_dataset = BSDS300(scale_factor=scale_factor)
-val_dataset = Set14(scale_factor=scale_factor)
-test_dataset = Set5(scale_factor=scale_factor)
+    def forward(self, x):
+        return self.maxpool_conv(x)
 
-# train_dataset = MyDataset()
 
-# train_dataloader = DataLoader(dataset=train_dataset, batch_size=4, shuffle=True)
-train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=1)
-test_dataloader = DataLoader(test_dataset, batch_size=1)
+class BridgeDown(nn.Module):
+    """Downscale bottleneck block: maxpool -> conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
-# Define model
-channels = 3
-model = SRResNet(scale_factor, channels)
-module = Module(model)
+    def forward(self, x):
+        return self.maxpool_conv(x)
 
-trainer = pl.Trainer(max_epochs=50)
-trainer.fit(
-    module,
-    train_dataloader,
-    val_dataloader,
-)
-module.eval()
-trainer.test(module, test_dataloader)
+
+class BridgeUP(nn.Module):
+    """Downscale bottleneck block: conv -> transpose conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv_up = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.conv_up(x)
+
+
+class UpBlock(nn.Module):
+    """Upscale block: double conv block -> transpose conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = DoubleConvBlock(in_channels * 2, in_channels)
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x1, x2):
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return torch.relu(self.up(x))
+
+
+class OutputBlock(nn.Module):
+    """Output block: double conv block -> output conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.out_conv = nn.Sequential(
+            DoubleConvBlock(in_channels * 2, in_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1))
+
+    def forward(self, x1, x2):
+        x = torch.cat([x2, x1], dim=1)
+        return self.out_conv(x)
+
+
+class DeepWBNet(nn.Module):
+    def __init__(self):
+        super(DeepWBNet, self).__init__()
+        self.n_channels = 3
+        self.encoder_inc = DoubleConvBlock(self.n_channels, 24)
+        self.encoder_down1 = DownBlock(24, 48)
+        self.encoder_down2 = DownBlock(48, 96)
+        self.encoder_down3 = DownBlock(96, 192)
+        self.encoder_bridge_down = BridgeDown(192, 384)
+        self.awb_decoder_bridge_up = BridgeUP(384, 192)
+        self.awb_decoder_up1 = UpBlock(192, 96)
+        self.awb_decoder_up2 = UpBlock(96, 48)
+        self.awb_decoder_up3 = UpBlock(48, 24)
+        self.awb_decoder_out = OutputBlock(24, self.n_channels)
+        self.tungsten_decoder_bridge_up = BridgeUP(384, 192)
+        self.tungsten_decoder_up1 = UpBlock(192, 96)
+        self.tungsten_decoder_up2 = UpBlock(96, 48)
+        self.tungsten_decoder_up3 = UpBlock(48, 24)
+        self.tungsten_decoder_out = OutputBlock(24, self.n_channels)
+        self.shade_decoder_bridge_up = BridgeUP(384, 192)
+        self.shade_decoder_up1 = UpBlock(192, 96)
+        self.shade_decoder_up2 = UpBlock(96, 48)
+        self.shade_decoder_up3 = UpBlock(48, 24)
+        self.shade_decoder_out = OutputBlock(24, self.n_channels)
+
+    def forward(self, x):
+        x1 = self.encoder_inc(x)
+        x2 = self.encoder_down1(x1)
+        x3 = self.encoder_down2(x2)
+        x4 = self.encoder_down3(x3)
+        x5 = self.encoder_bridge_down(x4)
+        x_awb = self.awb_decoder_bridge_up(x5)
+        x_awb = self.awb_decoder_up1(x_awb, x4)
+        x_awb = self.awb_decoder_up2(x_awb, x3)
+        x_awb = self.awb_decoder_up3(x_awb, x2)
+        awb = self.awb_decoder_out(x_awb, x1)
+        x_t = self.tungsten_decoder_bridge_up(x5)
+        x_t = self.tungsten_decoder_up1(x_t, x4)
+        x_t = self.tungsten_decoder_up2(x_t, x3)
+        x_t = self.tungsten_decoder_up3(x_t, x2)
+        t = self.tungsten_decoder_out(x_t, x1)
+        x_s = self.shade_decoder_bridge_up(x5)
+        x_s = self.shade_decoder_up1(x_s, x4)
+        x_s = self.shade_decoder_up2(x_s, x3)
+        x_s = self.shade_decoder_up3(x_s, x2)
+        s = self.shade_decoder_out(x_s, x1)
+        return torch.cat((awb, t, s), dim=1)
---
+++ Semester 5/Research Project/Video_Project/vsr.py
@@ -0,0 +1,146 @@
+import cv2
+import numpy as np
+import torch
+import torch.nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.transforms.functional as TF
+import pytorch_lightning as pl
+
+from torch_enhance.datasets import BSDS500, Set14, Set5, BSDS100
+from torch_enhance.models import SRResNet
+from torch_enhance import metrics
+
+from skimage.metrics import structural_similarity as ssim
+
+
+class Module(pl.LightningModule):
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def training_step(self, batch, batch_idx):
+        lr, hr = batch
+        sr = self(lr)
+        loss = F.mse_loss(sr, hr, reduction="mean")
+
+        # metrics
+        mae = metrics.mae(sr, hr)
+        psnr = metrics.psnr(sr, hr)
+
+        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+
+        ssim_value = np.mean(np.array([
+            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
+                 use_sample_covariance=False, win_size=3,
+                 data_range=sr_i.max() - sr_i.min())
+            for hr_i, sr_i in zip(hr_np, sr_np)
+        ]))
+
+        # Logs
+        self.log("train_loss", loss)
+        self.log("train_mae", mae)
+        self.log("train_psnr", psnr)
+        self.log("train_ssim", ssim_value)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        lr, hr = batch
+        sr = self(lr)
+        loss = F.mse_loss(sr, hr, reduction="mean")
+
+        # metrics
+        mae = metrics.mae(sr, hr)
+        psnr = metrics.psnr(sr, hr)
+
+        # Logs
+        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+
+        ssim_value = np.mean(np.array([
+            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
+                 use_sample_covariance=False, win_size=3,
+                 data_range=sr_i.max() - sr_i.min())
+            for hr_i, sr_i in zip(hr_np, sr_np)
+        ]))
+
+        # Logs
+        self.log("val_loss", loss)
+        self.log("val_mae", mae)
+        self.log("val_psnr", psnr)
+        self.log("val_ssim", ssim_value)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        lr, hr = batch
+        sr = self(lr)
+        loss = F.mse_loss(sr, hr, reduction="mean")
+
+        # metrics
+        mae = metrics.mae(sr, hr)
+        psnr = metrics.psnr(sr, hr)
+
+        # Logs
+        hr_np = hr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+        sr_np = sr.detach().cpu().numpy().transpose(0, 2, 3, 1)
+
+        for i, (hr_i, sr_i) in enumerate(zip(hr_np, sr_np)):
+            # Convert from numpy array to PIL Image
+            hr_i = (hr_i * 255).astype(np.uint8)
+            sr_i = (sr_i * 255).astype(np.uint8)
+            cv2.imwrite(f"hr_image_{batch_idx}.png", cv2.cvtColor(hr_i, cv2.COLOR_RGB2BGR))
+            cv2.imwrite(f"sr_image_{batch_idx}.png", cv2.cvtColor(sr_i, cv2.COLOR_RGB2BGR))
+
+        ssim_value = np.mean(np.array([
+            ssim(hr_i, sr_i, multichannel=True, gaussian_weights=True, sigma=1.5,
+                 use_sample_covariance=False, win_size=3,
+                 data_range=sr_i.max() - sr_i.min())
+            for hr_i, sr_i in zip(hr_np, sr_np)
+        ]))
+
+        # Logs
+        self.log("test_loss", loss)
+        self.log("test_mae", mae)
+        self.log("test_psnr", psnr)
+        self.log("test_ssim", ssim_value)
+
+        return loss
+
+
+scale_factor = 4
+
+# Setup dataloaders
+train_dataset = BSDS500(scale_factor=scale_factor)
+val_dataset = BSDS100(scale_factor=scale_factor)
+test_dataset = Set14(scale_factor=scale_factor)
+
+# train_dataset = MyDataset()
+
+# train_dataloader = DataLoader(dataset=train_dataset, batch_size=4, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=4)
+test_dataloader = DataLoader(test_dataset, batch_size=1)
+
+# Define model
+channels = 3
+model = SRResNet(scale_factor, channels)
+module = Module(model)
+
+trainer = pl.Trainer(max_epochs=25)
+trainer.fit(
+    module,
+    train_dataloader,
+    val_dataloader,
+)
+module.eval()
+trainer.test(module, test_dataloader)
