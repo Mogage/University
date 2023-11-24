d0535a3270fde5bbbe13ffa7081a4933c25f60a3
Nicolae Mogage <mogagenicolae@yahoo.com>
2023-11-24 12:20:28 +0200

    Changed deepwbnet training

--- Semester 5/Research Project/Video_Project/main.py
+++ Semester 5/Research Project/Video_Project/main.py
@@ -1,15 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import cv2
 import os
 from skimage.metrics import structural_similarity as ssim
+from sklearn.model_selection import train_test_split
+import pytorch_lightning as pl
 
 
 class DoubleConvBlock(nn.Module):
     """double conv layers block"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.double_conv = nn.Sequential(
@@ -25,6 +29,7 @@ class DoubleConvBlock(nn.Module):
 
 class DownBlock(nn.Module):
     """Downscale block: maxpool -> double conv block"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
@@ -38,6 +43,7 @@ class DownBlock(nn.Module):
 
 class BridgeDown(nn.Module):
     """Downscale bottleneck block: maxpool -> conv"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
@@ -52,6 +58,7 @@ class BridgeDown(nn.Module):
 
 class BridgeUP(nn.Module):
     """Downscale bottleneck block: conv -> transpose conv"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv_up = nn.Sequential(
@@ -66,6 +73,7 @@ class BridgeUP(nn.Module):
 
 class UpBlock(nn.Module):
     """Upscale block: double conv block -> transpose conv"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv = DoubleConvBlock(in_channels * 2, in_channels)
@@ -79,6 +87,7 @@ class UpBlock(nn.Module):
 
 class OutputBlock(nn.Module):
     """Output block: double conv block -> output conv"""
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.out_conv = nn.Sequential(
@@ -88,6 +97,15 @@ class OutputBlock(nn.Module):
     def forward(self, x1, x2):
         x = torch.cat([x2, x1], dim=1)
         return self.out_conv(x)
+
+
+class ChannelReductionModel(nn.Module):
+    def __init__(self):
+        super(ChannelReductionModel, self).__init__()
+        self.reduce_channels = nn.Conv2d(9, 3, kernel_size=1)
+
+    def forward(self, x):
+        return self.reduce_channels(x)
 
 
 class DeepWBNet(nn.Module):
@@ -144,17 +162,32 @@ class MyDataset(torch.utils.data.Dataset):
         self.inputs = []
         self.targets = []
         self.transform = transform
-        for filename in os.listdir('Set1_input_images_JPG/'):
-            img = cv2.imread('Set1_input_images_JPG/' + filename)
+        max_no_of_photos = 500
+        index = 0
+        image_size = (512, 512)
+        for filename in os.listdir('Set2_input_images/'):
+            img = cv2.imread('Set2_input_images/' + filename)
+            img = cv2.resize(img, image_size)
             img = img.transpose((2, 0, 1)).astype(np.float32)
             self.inputs.append(img)
-        for filename in os.listdir('Set1_ground_truth_images'):
-            img = cv2.imread('Set1_ground_truth_images' + filename)
+            index += 1
+            if index == max_no_of_photos:
+                break
+        index = 0
+        for filename in os.listdir('Set2_ground_truth_images/'):
+            img = cv2.imread('Set2_ground_truth_images/' + filename)
+            img = cv2.resize(img, image_size)
             img = img.transpose((2, 0, 1)).astype(np.float32)
             self.targets.append(img)
+            index += 1
+            if index == max_no_of_photos:
+                break
 
         self.inputs = np.array(self.inputs)
         self.targets = np.array(self.targets)
+
+        # self.inputs_train, self.inputs_test, self.targets_train, self.targets_test = train_test_split(
+        #     self.inputs, self.targets, test_size=0.2)
 
         self.inputs = torch.tensor(self.inputs, dtype=torch.float32)
         self.targets = torch.tensor(self.targets, dtype=torch.float32)
@@ -172,36 +205,66 @@ class MyDataset(torch.utils.data.Dataset):
         return item, target
 
 
-model = DeepWBNet()
+class Module(pl.LightningModule):
+    def __init__(self, model):
+        super(Module, self).__init__()
+        self.model = model
+        self.reshaper = ChannelReductionModel()
 
-# Define the loss function
-criterion = nn.MSELoss()
+    def forward(self, x):
+        return self.model(x)
 
-# Define the optimizer
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
 
-num_epochs = 10  # adjust as needed
+    def training_step(self, batch, batch_idx):
+        input_img, target = batch
 
-train_dataset = MyDataset()
+        output = self(input_img)
+
+        output = self.reshaper(output)
+
+        loss = F.mse_loss(output, target)
+
+        return loss
+
+
+num_epochs = 5
+
+dataset = MyDataset()
+train_indices, test_indices = train_test_split(range(len(dataset)), test_size=0.2, random_state=42)
+
+train_dataset = torch.utils.data.Subset(dataset, train_indices)
+test_dataset = torch.utils.data.Subset(dataset, test_indices)
 train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-test_dataset = MyDataset()
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-for epoch in range(num_epochs):
-    model.train()
-    running_loss = 0.0
+model = DeepWBNet()
 
-    for batch_idx, (inputs, targets) in enumerate(train_dataloader):
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+optimizer = optim.Adamax(model.parameters(), lr=1e-3)
 
-        running_loss += loss.item()
+# criterion = nn.MSELoss()
+# for epoch in range(num_epochs):
+#     model.train()
+#     running_loss = 0.0
+#
+#     for batch_idx, (inputs, targets) in enumerate(train_dataloader):
+#         optimizer.zero_grad()
+#         outputs = model(inputs)
+#         loss = criterion(outputs, targets)
+#         loss.backward()
+#         optimizer.step()
+#
+#         running_loss += loss.item()
+#
+#     average_loss = running_loss / len(train_dataloader)
+#     print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {average_loss}")
 
-    average_loss = running_loss / len(train_dataloader)
-    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {average_loss}")
+trainer = pl.Trainer(max_epochs=num_epochs)
+module = Module(model)
+trainer.fit(module, train_dataloader)
+
+reshaper_model = ChannelReductionModel()
 
 model.eval()
 total_ssim = 0.0
@@ -209,8 +272,11 @@ total_ssim = 0.0
 with torch.no_grad():
     for batch_idx, (inputs, targets) in enumerate(test_dataloader):
         outputs = model(inputs)
+        outputs = reshaper_model(outputs)
         ssim_value = np.mean(np.array([
-            ssim(targets_i, output_i, multichannel=True)
+            ssim(targets_i, output_i, multichannel=True, gaussian_weights=True, sigma=1.5,
+                 use_sample_covariance=False, win_size=3,
+                 data_range=output_i.max() - output_i.min())
             for targets_i, output_i in zip(targets.cpu().numpy(), outputs.cpu().numpy())
         ]))
         total_ssim += ssim_value
